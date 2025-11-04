973cf802a26d9dc679b452017d1e921e66ac4cdb
Krzysztof Murias <krzychu0897@gmail.com>
2025-11-04 12:55:36 +0100

    ZAD2

---
+++ LAB1/ZAD2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class bankaccount
+{
+    public decimal saldo { get; private set; }
+    public string wlasciciel;
+
+
+    public bankaccount(string wlasciciel, decimal saldo)
+    {
+        this.wlasciciel = wlasciciel;
+        this.saldo = saldo;
+
+    }
+    public void wplata(decimal kwota)
+    {
+
+        saldo = saldo + kwota;
+        Console.WriteLine("Twoje saldo: " + saldo);
+    }
+
+
+    public void wyplata(decimal kwota)
+    {
+
+
+        if (kwota > saldo)
+        {
+            Console.WriteLine("Za malo srodkow na koncie");
+            return;
+        }
+        else
+        {
+            saldo = saldo - kwota;
+        }
+        Console.WriteLine("Twoje saldo: " + saldo);
+    }
+}
+
+public class program
+
+{
+    static void Main()
+    {
+        bankaccount konto = new bankaccount("Jan kowalski", 2000);
+        konto.wyplata(1000);
+        konto.wplata(5000);
+    }
+
+}
+
+
